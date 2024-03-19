c1fbf263c22203239230159097162465b684bea8
DesalegnSendek <desalegnsendek2@gmail.com>
2024-03-19 22:16:19 +0300

    Fixed buggs by trying from scratch again

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {Home } from  "./pages/Home";
+import { Offers  } from "./pages/Offers";
+import {Profile } from  "./pages/Profile";
+import {Signup } from  "./pages/Signup";
+import {SignIn } from  "./pages/SignIn";
+import { ForgotPassword } from "./pages/ForgotPassword";
+
+import { Header } from './components/Header';
+import { Footer } from './components/Footer';
+
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+      <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/offers" element={<Offers />} />
+          <Route path="/sign-up" element={<Signup />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   );
 }
